c7be575af6cf5beed1ec718d6324b3e3666d2910
Vikram Aditya <vikramadityacr@gmail.com>
2023-10-20 12:55:29 +0530

    add the program to print the pattern

---
+++ pattern.py
@@ -0,0 +1,16 @@
+# Ask the user for an input for how many lines should be printed in the pattern
+n = 5
+
+# Define a function that takes an integer n and prints a pattern
+def pypattern(n):
+    # define the number of rows in the pattern
+    for i in range(0, n):
+        # define the number of stars to print on each row of the pattern
+        for j in range(0, i+1):
+            # print stars and end the line after each row
+            print("* ",end="")
+        # ending line after each row to make different rows for the pattern
+        print("\r")
+ 
+# Print the final pattern
+pypattern(n)
