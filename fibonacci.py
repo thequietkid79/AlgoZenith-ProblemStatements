75cfc4c7268ec2c288e8c10e1236d59defd37535
Vikram Aditya <vikramadityacr@gmail.com>
2023-10-20 12:23:20 +0530

    Add the program to print the fibonacci series

---
+++ fibonacci.py
@@ -0,0 +1,33 @@
+# function to print the Fibonacci series up to n terms
+def fibonacci(n):
+    # initialize variables
+    a, b = 0, 1
+    count = 0
+
+    # check if the number of terms is valid
+    if n <= 0:
+        print("Please enter a positive integer")
+    elif n == 1:
+        print("Fibonacci sequence upto",n,":")
+        print(a)
+    else:
+        print("Fibonacci sequence:")
+        while count < n:
+            print(a)
+            c = a + b
+            # update values
+            a = b
+            b = c
+            count += 1
+
+# take input from the user
+n = int(input("Enter the number of terms: "))
+
+# define the limits and print the output using the fibonacci function
+if n < 0:
+    print("Please enter a positive integer")
+elif n == 0:
+    print("Fibonacci sequence upto",n,":", "0")
+else:
+    print("Fibonacci sequence upto",n,":")
+    fibonacci(n)
