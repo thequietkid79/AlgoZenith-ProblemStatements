a3b66242a6b74dd7aefbb77ca25f9b26fa37a4a5
Vikram Aditya <vikramadityacr@gmail.com>
2023-10-20 12:46:40 +0530

    modify the fibonacci.py program to make it more refined

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,19 @@
-# function to print the Fibonacci series up to n terms
+# take input from the user
+n = int(input("Enter the number of terms: "))
+
+# define the fibonacci function which prints the Fibonacci series up to n terms
 def fibonacci(n):
     # initialize variables
     a, b = 0, 1
     count = 0
 
     # check if the number of terms is valid
-    if n <= 0:
+    if n < 0:
         print("Please enter a positive integer")
+    elif n == 0:
+        print("Fibonacci sequence upto",n,":",a)
     elif n == 1:
-        print("Fibonacci sequence upto",n,":")
-        print(a)
+        print("Fibonacci sequence upto",n,":",b)
     else:
         print("Fibonacci sequence:")
         while count < n:
@@ -20,14 +24,5 @@ def fibonacci(n):
             b = c
             count += 1
 
-# take input from the user
-n = int(input("Enter the number of terms: "))
-
-# define the limits and print the output using the fibonacci function
-if n < 0:
-    print("Please enter a positive integer")
-elif n == 0:
-    print("Fibonacci sequence upto",n,":", "0")
-else:
-    print("Fibonacci sequence upto",n,":")
-    fibonacci(n)
+# print the output using the fibonacci function
+fibonacci(n)
